283542c2f258063a91b2a88b9f7712b4db619a68
SamuelOliveira14 <samuel.augusto.sa@gmail.com>
2024-10-25 18:13:29 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -2,11 +2,14 @@ import express from 'express'
 import mid from './mid.js'
 import dotenv from 'dotenv'
 import axios from 'axios'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 8080
+
+app.use(cors())
 
 app.listen(PORT, () => {
     console.log(`Server running on: ${PORT}`)
